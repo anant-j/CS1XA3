48150f9e5dc36ff1f811d8b03a1790fd0fd3482d
Anant Jain <jaina17@mills.cas.mcmaster.ca>
2018-02-23 13:35:49 -0500

    Added files for Assignment 1

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -11,29 +11,6 @@ git diff >> changes.log
 grep -r --exclude="todo.log" "#TODO" * >> todo.log
 
 #Part 4
-find . -iname "*.hs" -exec ghc -fno-code {} \; >> error.log
+ghc -fno-code *.hs $1 &>> error.log
 
 #Part 5
-DIALOG=${DIALOG=Xdialog}
-tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
-trap "rm -f $tempfile" 0 1 2 5 15
-
-$DIALOG --title "1XA3-Input Box" --clear \
-        --inputbox "Hi, this is an input box\n
-Try entering :" 16 51 2> $tempfile
-
-retval=$?
-
-case $retval in
-  0)
-    echo "Input string is `cat $tempfile`";;
-  1)
-    echo "Cancel pressed.";;
-  255)
-    if test -s $tempfile ; then
-      cat $tempfile
-    else
-      echo "ESC pressed."
-    fi
-    ;;
-esac
