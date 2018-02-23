1d997b413bec85aaec9fce58f9d98237f696e384
Anant Jain <jaina17@mills.cas.mcmaster.ca>
2018-02-23 02:21:07 -0500

    Added files for Assignment 1

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -11,4 +11,4 @@ git diff >> changes.log
 grep -r --exclude="todo.log" "#TODO" * >> todo.log
 
 #Part 4
-ghc -fno-code "*".hs >> error.log
+find . -iname "*.hs" -exec ghc -fno-code {} \; >> error.log
