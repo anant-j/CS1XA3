b69bcdd5fd158663abd39c60a22a5ba8eb4b1846
Anant Jain <jaina17@mills.cas.mcmaster.ca>
2018-02-24 22:03:13 -0500

    Added files for Assignment 1

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -14,27 +14,8 @@ grep -r --exclude="todo.log" "#TODO" * >> todo.log
 ghc -fno-code *.hs $1 &>> error.log
 
 #Part 5
-echo "Would you like a print-out of the created files? (Y/N)" | boxes
+echo "Enter characters to be reversed and printed in a temporary file?" | boxes
 while read response;
 do
-	if [ ${response} = "Y" ] || [ ${response} = "y" ]
-	then
-		echo
-		echo -e "${BMAG}Contents of changes.log:${NORM}${REV}"
-		cat changes.log
-		echo -e "${NORM}"
-		echo -e "${BMAG}Contents of todo.log:${NORM}${REV}"
-                cat todo.log
-                echo -e "${NORM}"
-		echo -e "${BMAG}Contents of error.log:${NORM}${REV}"
-                cat error.log
-                echo -e "${NORM}"
-		break
-	elif [ ${response} = "N" ] || [ ${response} = "n" ]
-	then
-		echo "Thanks for using the Project Analyzer."
-		break
-	else
-		echo -e "${BRED}Please enter either Y for yes or N for no.${NORM}"
-	fi
+	echo  ${response}|rev
 done
