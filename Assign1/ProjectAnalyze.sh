76d32d1357df9f53f323d62593c34e4cb47684d9
Anant Jain <jaina17@mills.cas.mcmaster.ca>
2018-02-25 03:26:44 -0500

    Added files for Assignment 1

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -22,16 +22,16 @@ then
 	echo "$string IS a Palindrome, a file named $string would be created"
 	touch $string.txt
 	today=`date +%Y-%m-%d`
-	echo "This file was created on $today" >>$string.txt
 	FILE1=changes.log
 	FILES1="$(wc -c <$FILE1)"	
-	echo "Size of $FILE1 = $FILES1 bytes." >>$string.txt
 	FILE2=todo.log
         FILES2="$(wc -c <$FILE2)"
-        echo "Size of $FILE2 = $FILES2 bytes." >>$string.txt
-	FILE3=error.log
+        FILE3=error.log
         FILES3="$(wc -c <$FILE3)"
-        echo "Size of $FILE3 = $FILES3 bytes." >>$string.txt 
+        echo "This file was created on $today" >>$string.txt
+	echo "Size of "$FILE1" = $FILES1 bytes." >>$string.txt
+	echo "Size of "$FILE2" = $FILES2 bytes." >>$string.txt
+	echo "Size of "$FILE3" = $FILES3 bytes." >>$string.txt 
 	git add $string.txt
 	git commit -m"Added files for Assign 1"
 else
