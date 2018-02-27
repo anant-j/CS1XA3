a81861eb78f9535b55a77fd43d6667d8d0a90819
Anant Jain <jaina17@mills.cas.mcmaster.ca>
2018-02-26 19:31:48 -0500

    Added files for Assignment 1

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -11,6 +11,7 @@ git diff > changes.log
 grep -r --exclude="todo.log" "#TODO" * > todo.log
 
 #Part 4
+
 find . -name "*.hs" -exec ghc -fno-code {} \; &> error.log 
 
 #Part 5
@@ -27,21 +28,13 @@ then
 	FILES1="$(wc -c <$FILE1)"	
 	FILE2=todo.log
         FILES2="$(wc -c <$FILE2)"
-        FILE3=error.log
+	FILE3=error.log
         FILES3="$(wc -c <$FILE3)"
         echo "This file was created on $today" >>$string.txt
 	echo "Size of "$FILE1" = $FILES1 bytes." >>$string.txt
 	echo "Size of "$FILE2" = $FILES2 bytes." >>$string.txt
-	echo "Size of "$FILE3" = $FILES3 bytes." >>$string.txt 
-	echo Do you want to commit this newly created $string.txt file to local repository ?"(Y/N)":
-	read -n 1 option
-	if [ "$({option,})"="y" ]
-	then
-        	git add $string.txt
-        	git commit -m"Added file $string.txt for Assignment 1"
-	else
-        	echo Ok,the file wont be added and committed to the local repo.
-	fi
+	echo "Size of "$FILE3" = $FILES3 bytes." >>$string.txt
+        git add $string.txt
 else
 	echo "$string IS NOT a Palindrome.A file won't be created"
 fi
---
+++ Assign1/Removal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm changes.log
+rm error.log
+rm todo.log
