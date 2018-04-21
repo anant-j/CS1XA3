f1d4afc746b579b46b00c691dfba7c2d9096369a
jaina17 <35265303+jaina17@users.noreply.github.com>
2018-04-20 22:11:40 -0400

    Added Files for Assignment 3

---
+++ Assign3/ExprDiff.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module ExprDiff where
+
+  import ExprType
+  import ExprEval
+  import ExprPretty
+  import Data.Map as Map
+
+  class ExprDiff a where
+    partDiff :: String -> Expr a -> Expr a
+
+  instance ExprDiff Double where
+    partDiff v (Add e1 e2)  = Add (partDiff v e1) (partDiff v e2)
+    partDiff v (Sub e1 e2)  = Sub (partDiff v e1) (partDiff v e2)
+    partDiff v (Mult e1 e2) = let
+      pd1 = partDiff v e1
+      pd2 = partDiff v e2
+      in Add (Mult pd1 e2) (Mult e1 pd2)
+
+    partDiff v (Div (Const 1) e) = Mult (Mult (Const (-1)) (partDiff v e)) (Div (Const 1) (Pow e (Const 2)))
+    partDiff v (Div e1 e2)       = let
+      pd1 = partDiff v e1
+      pd2 = partDiff v e2
+      top = Sub (Mult pd1 e2) (Mult e1 pd2)
+      bottom = Pow (e2) (Const 2)
+      in (Div top bottom)
+
+    partDiff v (E e)        = Mult (E e) (partDiff v e)
+    partDiff v (Log a e)    = Mult (Div (Const 1) (Ln (Const a))) (partDiff v e)
+
+    partDiff v (Ln e)       = Mult (partDiff v e) (Div (Const 1) e)
+
+    partDiff v (Cos e)      = Mult (partDiff v e) (Mult (Const (-1)) (Sin e))
+    partDiff v (Sin e)      = Mult (partDiff v e) (Cos e)
+    partDiff v (Pow e1 e2)  = partDiff v (E (Mult e2 (Ln e1)))
+
+    partDiff _ (Const a)    = Const 0
+    partDiff v (Var b)      = if b == v then (Const 1) else (Const 0)
---
+++ Assign3/ExprParser.hs
@@ -0,0 +1,210 @@
+module ExprParser (parseExprD,parseExprF,parseExprI,parseExprInt) where
+
+import ExprType
+
+import Text.Parsec
+import Text.Parsec.String
+
+{-Parse Exprs
+    ---------------------
+    Takes a string of format:
+    and parses an expression of Expr + 
+-}
+
+-- * Parsers
+
+-- ** Main Parsers
+
+
+parseExprD :: String -- ^ The string to be parsed 
+            -> Expr Double -- ^ The resulting parsed Double Expr
+parseExprD ss = case parse dFactor "" ("("++ss++")") of
+                    Left err -> error (show err)
+                    Right expr -> expr
+
+parseExprF:: String  -- ^ The string to be parsed 
+            -> Expr Float -- ^ The resulting parsed Float Expr
+parseExprF ss = case parse fFactor "" ("("++ss++")") of
+                    Left err -> error (show err)
+                    Right expr -> expr
+
+parseExprI:: String -- ^ The string to be parsed
+            -> Expr Integer -- ^ The resulting parsed Integer Expr
+parseExprI ss = case parse iFactor "" ("("++ss++")") of 
+                    Left err -> error (show err)
+                    Right expr -> expr
+
+parseExprInt :: String -- ^ The string to be parsed
+            -> Expr Int -- ^ The resulting parsed Int Expr
+parseExprInt ss = case parse intFactor "" ("("++ss++")") of
+                    Left err -> error (show err)
+                    Right expr -> expr
+
+-- ** Main parser Helpers
+
+iFactor :: Parser (Expr Integer)
+iFactor = try (parens $ expr iFactor) <|> try iConst <|> var
+
+iConst = do {i <- integer;
+            return (Const i)}
+
+intFactor :: Parser (Expr Int)
+intFactor = try (parens $ expr intFactor) <|> try intConst <|> var
+
+intConst = do {i <- int;
+            return (Const i)}
+
+dFactor :: Parser (Expr Double)
+dFactor = try (parens $ expr dFactor) <|> try dConst <|> var
+
+dConst = do {d <- double;
+            return (Const d)}
+
+fFactor :: Parser (Expr Float)
+fFactor = try (parens $ expr fFactor) <|> try fConst <|> var
+
+fConst = do {f <- float;
+            return (Const f)}
+
+-- ** Utility parsers
+{-Common parsers, common to most of the parser stuffs-}
+
+-- | attempt to parse an identifier for a variable consisting of alpha-numerical characters
+iden :: Parser String
+iden = many1 alphaNum
+
+-- | attempt to parse a variable consisting of a potentially space padded identifier
+var :: Parser (Expr a)
+var = do {  spaces;
+            ss <-iden;
+            spaces;
+            return (Var ss)}
+
+-- | attempt to parse a multiplication operation consisting of the symbol !*
+multOp :: Parser (Expr a -> Expr a -> Expr a)
+multOp = do {symbol "!*";
+            return (Mult)}
+
+-- | attempt to parse an addition operation consisting of the symbol !+
+addOp :: Parser (Expr a -> Expr a -> Expr a)
+addOp = do {symbol "!+";
+            return (Add)}
+
+-- | attempt to parse an exponentiation operation consisting of the symbol !^
+expOp :: Parser (Expr a -> Expr a -> Expr a)
+expOp = do {symbol "!^";
+            return (Exp)}
+
+-- generic trig operation stuffs cause only diff = log, vs sin, vs cos etc
+trigOp :: String ->
+            (Expr a -> Expr a) ->
+            Parser (Expr a) ->
+            Parser (Expr a)
+trigOp name val root = do {symbol name;
+                            expr <- root;
+                         return (val expr)}
+-- | Attempt to parse an expression consisting of terms separated by addition operations
+expr :: Parser (Expr a) -> Parser (Expr a)
+expr factor = (term factor) `chainl1` addOp
+-- | Attempt to parse a term consisting of trig expressions separated by multiplication operations
+term :: Parser (Expr a) -> Parser (Expr a)
+term factor = (expo factor) `chainl1` multOp
+
+expo :: Parser (Expr a) -> Parser (Expr a)
+expo factor = (trig factor) `chainl1` expOp
+{- | Attemtpt to parse a trig operation which is any operation of the form `name(Expr...)` wherein name
+    of the trig operation and Expr... is the nestled expressions within
+-}
+trig :: Parser (Expr a) -> Parser (Expr a)
+trig factor = try (trigOp "cos" Cos factor) 
+            <|> try (trigOp "sin" Sin factor) 
+            <|> try (trigOp "ln" Ln factor) 
+            <|> try (trigOp "e^" NExp factor)
+            <|> factor
+
+
+{- Utility parsers
+    parens : surrounds parser in round braces
+    symbol : parses some string that may be surrounded by spaces
+    digits : parses a positive int or fails
+    negDigits: parses a negative int or fails
+    dec : parses the decimal aka numbers after the `.` including `.`
+    mantissa : parses the number(+/-) after the `e` including `e`
+    nilStr : A parser that simply returns the empty string ""
+    decimal : parses a decimal in the general form `#.#e?#` # represenging any number of digits and ? indicating a potential position for `-`
+    negDecimal : same as Decimal except in the form `-#.#e?#`
+    float : parses a float of the form `?#.#e?#` # represenging any number of digits and ? indicating a potential position for `-`
+    double : parses a double of the form `?#.#e?#` # represenging any number of digits and ? indicating a potential position for `-
+    integer : parses a positive or negative integer or fails
+    int : parses a number to a machine integer or fails
+-}
+-- ** Generic parsers
+
+-- *** General utility
+
+parens :: Parser a -> Parser a
+parens p = do { symbol "(";
+                cs <- p;
+                symbol ")";
+                return cs }
+
+
+symbol :: String -> Parser String
+symbol ss = let
+  symbol' :: Parser String
+  symbol' = do { spaces;
+                 ss' <- string ss;
+                 spaces;
+                 return ss' }
+  in try symbol'
+
+-- *** Numeric Utility
+
+digits :: Parser String
+digits = many1 digit
+
+negDigits :: Parser String
+negDigits = do { neg <- symbol "-" ;
+                 dig <- digits ;
+                 return (neg ++ dig) }
+
+dec :: Parser String
+dec = do {
+            char '.';
+            a <- digits;
+            return ("."++a)}
+
+mantissa :: Parser String
+mantissa = do {
+            char 'e';
+            e <- (try negDigits<|>digits);
+            return ("e"++e);
+            }
+
+nilStr :: Parser String
+nilStr = do {return ""}
+
+decimal :: Parser String 
+decimal = do {r <- digits;
+                a <- (try dec<|>nilStr);
+                e <- (try mantissa<|>nilStr);
+            return (r++a++e)}
+
+negDecimal :: Parser String
+negDecimal = do {neg <- symbol "-";
+                dec <- decimal;
+            return (neg++dec)}
+
+-- *** Number Parsers
+
+float :: Parser Float
+float = fmap read $ try negDecimal <|> decimal
+
+double :: Parser Double
+double = fmap read $ try negDecimal <|> decimal
+
+integer :: Parser Integer
+integer = fmap read $ try negDigits <|> digits
+
+int :: Parser Int
+int = fmap read $ try negDigits <|> digits
