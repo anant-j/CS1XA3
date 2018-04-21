06704a1f28406cd24ed4cf6c3459c52c715d2fa9
jaina17 <35265303+jaina17@users.noreply.github.com>
2018-04-20 22:36:55 -0400

    Updated Files

--- Assign3/ExprPretty.hs
+++ Assign3/ExprPretty.hs
@@ -1,12 +1,18 @@
 module ExprPretty where
 
-import           ExprType
+import ExprType
 
+-- | To Wrap the expression in Parenthesis
 parens :: String -> String
 parens ss = "(" ++ ss ++ ")"
 
+-- | Formats each operation by printing the operator associated with that
 instance Show a => Show (Expr a) where
   show (Mult e1 e2) = parens (show e1) ++ " !* " ++ parens (show e2)
   show (Add e1 e2)  = parens (show e1) ++ " !+ " ++ parens (show e2)
   show (Var ss)     = parens $ "var \"" ++ ss ++ "\""
-  show (Const x)    = parens $ "val " ++ show x
+  show (Const x) = parens $ "val " ++ show x
+  show (Cos ss) = parens $ "cos " ++ show ss
+  show (Sin ss) = parens $ "sin " ++ show ss
+  show (Log ss) = parens $ "log " ++ show ss
+  show (NatExp ss) = parens $ "e^" ++ show ss
--- Assign3/ExprTest.hs
+++ Assign3/ExprTest.hs
@@ -8,15 +8,29 @@ import           ExprType
 import qualified Data.Map.Strict as Map
 import           Test.QuickCheck
 
-sampleExpr1 :: Expr Int
-sampleExpr1 = (var "x") !+ (var "y")
+sampleExpr1 :: Expr Double
+sampleExpr1 = ((var "x") !+ (var "y"))
+
 
 
 listToExpr1 :: [Double] -> Expr Double
 listToExpr1 [x]    = Const x
 listToExpr1 (x:xs) = Add (Const x) (listToExpr1 xs)
-listToExpr1 []     = error "Not list to expression for empty"
+listToExpr1 []     = error "list is empty"
 
 
-test1 :: Int -> Bool
-test1 x = eval (Map.fromList [("x",x),("y",-x)]) sampleExpr1 == 0
+evalProp1 :: Double -> Double -> Bool
+evalProp1 a b = eval (Map.fromList [("x",a),("y",b)]) (Add (Var "x") (Var "y")) == a + b
+testEvalProp1 = quickCheck evalProp1
+
+evalProp2 :: Double -> Double -> Bool
+evalProp2 a b = eval (Map.fromList [("x",a),("y",b)]) (Mult (Var "x") (Var "y")) == a * b
+testEvalProp2 = quickCheck evalProp2
+
+evalProp6 :: Double -> Bool
+evalProp6 a = eval (Map.fromList [("x",a)]) (Cos (Var "x")) == cos(a)
+testEvalProp6 = quickCheck evalProp6
+
+evalProp7 :: Double -> Bool
+evalProp7 a = eval (Map.fromList [("x",a)]) (Sin (Var "x")) == sin(a)
+testEvalProp7 = quickCheck evalProp7
